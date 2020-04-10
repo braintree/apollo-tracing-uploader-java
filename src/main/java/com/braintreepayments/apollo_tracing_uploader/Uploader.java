bd74a4ebdb70c054b87a6405fca8f56ec27b1ac9
Alex Genco <alexgenco@gmail.com>
2020-04-10 12:31:40 -0700

    fixup! Convert TraceProducer into an interface

--- src/main/java/com/braintreepayments/apollo_tracing_uploader/Uploader.java
+++ src/main/java/com/braintreepayments/apollo_tracing_uploader/Uploader.java
@@ -4,8 +4,5 @@ import mdg.engine.proto.Reports;
 
 @FunctionalInterface
 public interface Uploader {
-  String APOLLO_TRACING_URL = "https://engine-report.apollodata.com/api/ingress/traces";
-  String API_KEY_HEADER = "X-Api-Key";
-
   void upload(Reports.FullTracesReport report);
 }
